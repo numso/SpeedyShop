0f9a0e33c5d0b02b1fd589da0b18a51cad3c0be3
Dallin Osmun <dosmun@gmail.com>
2012-10-05 11:58:47 -0600

    added help pages to the login modal

--- public/js/pages/common/views/header.js
+++ public/js/pages/common/views/header.js
@@ -12,28 +12,79 @@ define([
     signedInTmpl
 ) {
     return Backbone.View.extend({
+        render: function () {
+            var that = this;
+
+            $.get('/getCategories/', function (data) {
+                that.$el.html(headerTmpl(data));
+
+                $.get('/getUserName', function (user) {
+                    if (user === '') {
+                        that.$('.account-box').html(loginModalTmpl());
+                    } else {
+                        that.$('.account-box').html(signedInTmpl({ user: user }));
+                    }
+                });
+            });
+
+            return this;
+        },
 
         initialize: function () {
         },
 
         events: {
-            "click .show-sub-cats": "showSubCats",
-            "click .show-items": "showItems",
-            "click #logIn": "showSignInScreen",
-            "click #logOut": "logOut",
-            "click .sign-in-modal": "stopPropagation",
-            "click .log-in": "logIn"
+            'click .show-sub-cats': 'showSubCats',
+            'click .show-items': 'showItems',
+            'click #show-log-in': 'showLogInScreen',
+            'click #show-sign-up': 'showSignUpScreen',
+            'click #log-in': 'logIn',
+            'click #sign-up': 'signUp',
+            'click #logOut': 'logOut',
+            'click .sign-in-modal': 'stopPropagation',
+
+            'focus input': 'showInfo',
+            'blur input': 'hideInfo',
         },
 
-        logOut: function (e) {
-            var that = this;
+        showInfo: function (e) {
+            var name = $(e.target).closest('input').attr('class');
+            console.log(name);
+            this.$('.' + name + '-info').addClass('show');
+        },
 
-            $.post('/logout', function (data) {
-                data = JSON.parse(data);
-                if (data.success) {
-                    that.$('.account-box').html(loginModalTmpl());
-                }
-            });
+        hideInfo: function (e) {
+            this.$('.info').removeClass('show');
+        },
+
+        // Menu Methods ////////////////////////////////////////////////////////////////////////
+
+        showSubCats: function (e) {
+            this.stopPropagation(e);
+            var myEl = $(e.target).closest('.show-sub-cats').find('.sub-cat-container');
+            myEl.slideToggle(100);
+            this.closeAllBut(myEl);
+        },
+
+        showItems: function (e) {
+            var itemName = $(e.target).closest('.show-items').text();
+            console.log('show items tagged with ' + itemName);
+        },
+
+        // Log-In Methods //////////////////////////////////////////////////////////////////////
+
+        showLogInScreen: function (e) {
+            this.$('.sign-in-modal').css('height', '170px');
+            this.$('.log-in-specific').removeClass('toggle-hidden');
+            this.closeAllBut('sign-in');
+            this.stopPropagation(e);
+        },
+
+        showSignUpScreen: function (e) {
+            this.$('.sign-in-modal').css('height', '360px');
+            this.$('.log-in-specific').addClass('toggle-hidden');
+            this.closeAllBut('sign-in');
+            this.stopPropagation(e);
         },
 
         logIn: function (e) {
@@ -47,62 +98,89 @@ define([
             $.post('/login', JSON.stringify(userObj), function (data) {
                 data = JSON.parse(data);
                 if (data.success) {
-                    that.$('.account-box').html(signedInTmpl({user: userObj.user}));
+                    that.$('.account-box').html(signedInTmpl({ user: data.user }));
                 }
             });
         },
 
-        showSignInScreen: function (e) {
-            this.$(".sign-in-modal").css("height", "300px");
-            this.closeAllBut("sign-in");
-            this.stopPropagation(e);
-        },
-
-        toggleSignUpScreen: function (e) {
-            this.$(".sign-in-modal").css("height", "600px");
-            this.closeAllBut("sign-in");
-            this.stopPropagation(e);
-        },
-
-        render: function () {
+        signUp: function (e) {
             var that = this;
-            $.get("/getCategories/", function (data) {
-                that.$el.html(headerTmpl(data));
 
-                that.$('.account-box').html(loginModalTmpl());
+            if (this.validateForm()) {
+                var userObj = {
+                    user: this.$('.user').val(),
+                    pass: this.$('.pass').val(),
+                    email: this.$('.email').val(),
+                    fname: this.$('.fname').val(),
+                    lname: this.$('.lname').val()
+                };
 
-                $.get("/getUserName", function (data) {
-                    if (data !== '') {
-                        that.$('.account-box').html(signedInTmpl({user: data}));
+                $.post('/signup', JSON.stringify(userObj), function (data) {
+                    data = JSON.parse(data);
+                    if (data.success) {
+                        that.$('.account-box').html(signedInTmpl({ user: data.user }));
                     }
                 });
+            }
+        },
+
+        validateForm: function () {
+            var myObjs = [
+                this.$('.user'),
+                this.$('.pass'),
+                this.$('.cpass'),
+                this.$('.email'),
+                this.$('.fname'),
+                this.$('.lname')
+            ];
+
+            var isValid = true;
+
+            for (var i = 0; i < myObjs.length; ++i) {
+                myObjs[i].removeClass('error');
+                if (myObjs[i].val() === '') {
+                    isValid = false;
+                    myObjs[i].addClass('error');
+                }
+            }
+
+            if (this.$('.pass').val() !== this.$('.cpass').val()) {
+                isValid = false;
+                this.$('.pass').addClass('error');
+                this.$('.cpass').addClass('error');
+            }
+
+            if (false) { // check email
+                isValid = false;
+            }
+
+            return isValid;
+        },
+
+        logOut: function (e) {
+            var that = this;
+
+            $.post('/logout', function (data) {
+                data = JSON.parse(data);
+                if (data.success) {
+                    that.$('.account-box').html(loginModalTmpl());
+                }
             });
-            return this;
         },
 
-
-        showSubCats: function (e) {
-            this.stopPropagation(e);
-            var myEl = $(e.target).closest(".show-sub-cats").find(".sub-cat-container");
-            myEl.slideToggle(100);
-            this.closeAllBut(myEl);
-        },
-
-        showItems: function (e) {
-            // load items on page
-        },
+        // Helper Methods //////////////////////////////////////////////////////////////////////
 
         closeAllBut: function (myMenu) {
-            var menus = $(".flyout");
+            var menus = $('.flyout');
             for (var i = 0; i < menus.length; ++i) {
                 if (menus[i] !== myMenu[0]) {
                     $(menus[i]).hide();
                 }
             }
 
-            // if (myMenu !== $(".sign-in-modal")) {
-            //     $(".sign-in-modal").css("height", 0);
-            // }
+            if (myMenu !== 'sign-in') {
+                this.$('.sign-in-modal').css('height', 0);
+            }
         },
 
         stopPropagation: function (e) {
@@ -110,6 +188,5 @@ define([
             if (e.cancelBubble) { e.cancelBubble = true; }
             else { e.stopPropagation(); }
         }
-
     });
 });
