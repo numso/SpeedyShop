615921267a2162b202f0337e0898a5911de2695c
Jesse <jvictors@jessevictors.com>
2012-11-02 21:59:58 -0600

    JS function wrote 70 new items :)

--- zzjunk/itemsConverter.js
+++ zzjunk/itemsConverter.js
@@ -12,7 +12,7 @@ var addIDs = function () {
 
 var updateImages = function () {
     var fs = require('fs'),
-        items = JSON.parse(fs.readFileSync('../serverData/newItems.json'));
+        items = JSON.parse(fs.readFileSync('../serverData/items.json'));
 
     for (var i = 0; i < items.length; ++i) {
         items[i].extraimages[0] = items[i].images;
@@ -21,10 +21,42 @@ var updateImages = function () {
         items[i].available = 5;
     }
 
-    fs.writeFileSync('../serverData/items.json', JSON.stringify(items));
+    fs.writeFileSync('../serverData/newItems.json', JSON.stringify(items));
+};
+
+var addMore = function () { //I wrote this! Ha! :)  -Jesse
+    var fs = require('fs');
+    var allItems = "";
+    for (var i = 24; i < 24 + 70; ++i)
+    {
+        var item = [
+            '   {',
+            '       "name": "Item '+i+'",',
+            '       "cat": [',
+            '           "Sports",',
+            '           "Posters"',
+            '       ],',
+            '       "price": 1234,',
+            '       "rating": 5,',
+            '       "desc": "Awesome item '+i+'",',
+            '       "images": [',
+            '           "//www.bmotorsports.com/shop/images/product-unavailable.jpg",',
+            '           "//www.bmotorsports.com/shop/images/product-unavailable.jpg",',
+            '           "//www.bmotorsports.com/shop/images/product-unavailable.jpg",',
+            '           "//www.bmotorsports.com/shop/images/product-unavailable.jpg"',
+            '       ],',
+            '       "id": '+i+',',
+            '       "available": 5',
+            '   },',
+            ''].join('\n');
+
+        allItems = allItems + item;
+    }
+
+    fs.writeFileSync('../serverData/newItems.json', allItems);
 };
 
 
 // just call the desired function right here :)
 //addIDs();
-updateImages();
+addMore();
