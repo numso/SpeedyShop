3aa79a2858ca2fbea1f4038477652d30d2b9e50d
Jesse <jvictors@jessevictors.com>
2012-11-03 17:47:14 -0600

    essentially finished filters

--- public/js/pages/customer/views/filters.js
+++ public/js/pages/customer/views/filters.js
@@ -2,10 +2,12 @@
 
 define([
     'backbone',
-    'tmpl!pages/customer/templates/filters'
+    'tmpl!pages/customer/templates/filters',
+    'tmpl!pages/customer/templates/items'
 ], function (
     Backbone,
-    filtersTmpl
+    filtersTmpl,
+    itemsTmpl
 ) {
     return Backbone.View.extend({
 
@@ -13,6 +15,35 @@ define([
         },
 
         events: {
+            "click .ratingFilterList li": "applyFilter",
+            "click .priceFilter button": "applyFilter",
+            "click .filter-value-box": "applyFilter"
+        },
+
+        applyFilter: function (e) {
+            //toggle checkbox if ratings image is clicked on
+            if ($(e.target).hasClass('ratingsImg')) {
+                var el = $(e.target).closest('li').find('input');
+                el.attr('checked', !el.attr('checked'));
+            }
+
+            if ($(e.target).hasClass('subcat-radio-value')) {
+                var el = $(e.target).find('input');
+                el.attr('checked', true);
+            }
+
+            var lowerPrice = this.$('.lowerPriceText').attr('value');
+            var upperPrice = this.$('.upperPriceText').attr('value');
+
+            var chosenRatings = [];
+            for (var i = 0; i < 5; ++i)
+                if ($(this.$('.ratingFilterList input')[i]).is(':checked'))
+                    chosenRatings.push(i + 1);
+
+            var chosenSubcat = $('input[name=subCatFilter]:radio:checked').attr('value');
+
+            var chosenFilters = [lowerPrice, upperPrice, chosenRatings, chosenSubcat];
+            console.log("applyFilter called, " + JSON.stringify(chosenFilters));
         },
 
         render: function () {
