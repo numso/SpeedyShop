dfb272b898f94b80e746c1a53c570fed0c8dd23a
Mauriel <mauriel.ramirez@gmail.com>
2012-11-10 15:25:35 -0700

    promocodes

--- public/js/pages/admin/views/promocodelist.js
+++ public/js/pages/admin/views/promocodelist.js
@@ -8,7 +8,7 @@ define([
     promocodelistTmpl
 ) {
     return Backbone.View.extend({
-
+        promocodeItems: [],
         initialize: function () {
         },
 
@@ -17,12 +17,23 @@ define([
 
         render: function () {
             this.$el.html(promocodelistTmpl());
+
             return this;
+        },
+
+        loadPromoCodes: function () {
+            var that = this;
+
+            // get the promocode items from the server
+            $.get('/promocodes/', function (items) {
+                // display the promocode items in a list
+                that.gotPromoCodes(items);
+            });
+        },
+
+        gotPromoCodes: function(items){
+            this.$('.promocode-list-module').append(promocodelistTmpl(this.items));
         }
 
-        loadPromoCodes: function(){
-
-            
-        }
     });
 });
