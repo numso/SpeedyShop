668a34f164ea21afd4e7ce29ef6279c2e2eb003c
Jesse <jvictors@jessevictors.com>
2012-11-11 20:06:15 -0700

    fancy tabs for item editing

--- public/js/pages/admin/views/items.js
+++ public/js/pages/admin/views/items.js
@@ -2,22 +2,48 @@
 
 define([
     'backbone',
-    'tmpl!pages/admin/templates/items'
+    'tmpl!pages/admin/templates/items',
+    'tmpl!pages/admin/templates/itemList/addItem',
+    'tmpl!pages/admin/templates/itemList/deleteItems',
+    'tmpl!pages/admin/templates/itemList/editItems'
 ], function (
     Backbone,
-    itemsTmpl
+    itemsTmpl,
+    addItemTmpl,
+    deleteItemsTmpl,
+    editItemsTmpl
 ) {
     return Backbone.View.extend({
 
+        myTmpls: undefined,
+        currentTab: undefined,
+
         initialize: function () {
+            currentTab = this.$('#tab-0');
+            this.myTmpls = [
+                addItemTmpl,
+                editItemsTmpl,
+                deleteItemsTmpl
+            ];
         },
 
         events: {
+            'click .tab': 'changeTab'
         },
 
         render: function () {
             this.$el.html(itemsTmpl());
+            this.$('.items-body').html(addItemTmpl);
             return this;
+        },
+
+        changeTab: function (e) {
+            var tab = this.$(e.target).closest('.tab');
+            currentTab.removeClass('active');
+            tab.addClass('active');
+            console.log(tab);
+            this.$('.items-body').html(this.myTmpls[tab.attr('id').charAt(4)]);
+            currentTab = tab;
         }
     });
 });
