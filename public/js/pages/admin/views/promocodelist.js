a38d1148612f65f00eccc3757df1962399361e32
justinkredible56 <justin_permann@hotmail.com>
2012-11-13 02:28:23 -0700

    Updated promocodeview and promocodelistview so it loads selected promo

--- public/js/pages/admin/views/promocodelist.js
+++ public/js/pages/admin/views/promocodelist.js
@@ -2,38 +2,46 @@
 
 define([
     'backbone',
-    'tmpl!pages/admin/templates/promocodelist'
+    'tmpl!pages/admin/templates/promocodelist',
+
 ], function (
     Backbone,
     promocodelistTmpl
 ) {
     return Backbone.View.extend({
-        promocodeItems: [],
+        codeList: undefined, 
         initialize: function () {
         },
 
         events: {
-            "click #add-new-btn": "addNew",
+            "click .add-new-btn": "addNew",
+            "click .row" : "openPromo"
         },
-        codeList: undefined, 
 
         render: function () {
             var that = this;
             $.get('/promocodes', function (items) {
                 that.codeList = JSON.parse(items);
-                console.log(that.codeList);
                 that.$el.html(promocodelistTmpl(that.codeList));
             });
             return this;
         },
 
         addNew: function () {
-            
+            console.log("im in here!");
         },
 
-        gotPromoCodes: function(items){
-            this.$('.promocode-list-module').append(promocodelistTmpl(this.items));
+        openPromo: function (e) {
+            var el = parseInt($(e.target).closest('.code-field').html(), 10);
+
+            for(var n = 0; n < this.codeList.length; ++n)
+                if(this.codeList[n].code == el)
+                    var pickedPromo = this.codeList[n];
+
+            this.model.showPromo(pickedPromo);
+
         }
+
 
     });
 });
