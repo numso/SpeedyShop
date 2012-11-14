900e2da11f3549be8143dadd5e142023178270ad
numso <dosmun@gmail.com>
2012-11-14 00:11:31 -0700

    added fake call for sales charts data so Fred can start formatting the client side

--- server.js
+++ server.js
@@ -78,3 +78,6 @@ app.post('/deleteItem/:itemID', itemServices.deleteItem);
 app.post('/editInventory', adminServices.editInventory);
 app.get('/itemList', itemServices.chartItems);
 app.get('/analytics', itemServices.chartSales);
+
+
+app.get('/tempSalesChartCall', adminServices.tempChartCall);
--- serverFiles/adminServices.js
+++ serverFiles/adminServices.js
@@ -59,6 +59,50 @@ module.exports = function () {
             response.send(newArr);
         },
 
+        tempChartCall: function (request, response, next) {
+            var year = 2012,
+                newArr = [];
+
+            for (var i = 0; i < sales.length; ++i) {
+                var newDate = new Date(sales[i].timestamp);
+                if (newDate.getFullYear() == year) {
+                    var actual = 0,
+                        projected = 0;
+                    for (var j = 0; j < sales[i].items.length; ++j) {
+                        actual += sales[i].items[j].actual;
+                        projected += sales[i].items[j].projected;
+                    }
+
+                    if (newArr[newDate.getMonth()]) {
+                        newArr[newDate.getMonth()].actual += actual;
+                        newArr[newDate.getMonth()].projected += projected;
+                    } else {
+                        newArr[newDate.getMonth()] = {
+                            actualGross: actual,
+                            projectedGross: projected,
+                            actualNet: actual,
+                            projectedNet: projected,
+                            actualQuantity: actual,
+                            projectedQuantity: projected
+                        };
+                    }
+                }
+            }
+
+            var returnVal = [];
+
+            var cats = JSON.parse(fs.readFileSync('serverData/cat.json'));
+            for (var j = 0; j < cats.length; ++j) {
+                if (cats[j].title !== "Hot Items") {
+                    returnVal.push({
+                        cat: cats[j].title,
+                        data: newArr
+                    });
+                }
+            }
+            response.send(returnVal);
+        },
+
         getPromoCodes: function (request, response, next) {
 
             var codes = fs.readFileSync('serverData/promoCodes.json');
@@ -77,13 +121,9 @@ module.exports = function () {
             });
 
             request.on('end', function () {
-            input = JSON.parse(input);
-
-            fs.writeFileSync('serverData/items.json', JSON.stringify(input));
+                input = JSON.parse(input);
+                fs.writeFileSync('serverData/items.json', JSON.stringify(input));
             });
-
-
         }
-
     };
 };
