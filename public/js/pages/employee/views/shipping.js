68524b292ee31489dcac5c6c24fd7efe5baba89a
numso <dosmun@gmail.com>
2012-11-13 23:39:16 -0700

    [fixed #39] added support for qr codes

--- public/js/pages/employee/views/shipping.js
+++ public/js/pages/employee/views/shipping.js
@@ -13,8 +13,8 @@ define([
         },
 
         events: {
-
-            'click .completed-btn': 'updateOrders'
+            'click .completed-btn': 'updateOrders',
+            'click .print-shipping-label': 'printLabel'
         },
 
         render: function () {
@@ -23,14 +23,40 @@ define([
             }));
             return this;
         },
+
+        printLabel: function (e) {
+            window.print();
+        },
+
+        showLabel: function () {
+            var data = {
+                name: 'bubba',
+                address: '575n 590w, Logan, UT, 84321',
+                'order#': this.pickedOrder.orderNum,
+                weight: this.pickedOrder.estimatedWeight
+            };
+
+            this.$('.ship-lbl-name').find('span').html(data.name);
+            this.$('.ship-lbl-addr').find('span').html(data.address);
+            this.$('.ship-lbl-order-num').find('span').html(data['order#']);
+            this.$('.ship-lbl-weight').find('span').html(data.weight);
+
+            var newData = '';
+            for (var i in data) {
+                newData += i + ":" + data[i] + '\n'
+            }
+            this.drawQRCode('shipping-qr', newData);
+        },
+
         gotOrder: function (data) {
             this.pickedOrder = data;
             this.$el.html(shippingTmpl(data));
+            this.showLabel();
         },
 
         updateOrders: function(){
             if(this.isValid())
-                {       
+                {
                     this.pickedOrder.orderStatus = "Completed";
                     console.log(JSON.stringify(this.pickedOrder));
                     $.post('/updateThisOrder', JSON.stringify(this.pickedOrder), function (resp) {
@@ -48,15 +74,24 @@ define([
 
 
         },
-        
+
         isValid: function(){
             flag = true;
             $('.item-check').each(function (index){
                 var thisValue = $(this).is(':checked');
                 if(!thisValue)
                     flag = false;
-           }); 
+           });
             return flag;
+        },
+
+        drawQRCode: function (id, data) {
+            var qrcodedraw = new QRCodeLib.QRCodeDraw();
+            qrcodedraw.draw(document.getElementById(id), data, function (error, canvas) {
+                if (error) {
+                    return console.log('ERROR: ' + error);
+                }
+            });
         }
     });
 });
